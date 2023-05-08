c61c7ea1ee96969549ee804d1a1b90982bf6a427
Mikael <mikael.coletto.eng@gmail.com>
2023-05-08 06:59:57 -0300

    Renomeando dfs;

--- R/fct-auxiliares/fct_ajustando_tags.R
+++ R/fct-auxiliares/fct_ajustando_tags.R
@@ -25,30 +25,37 @@ func_ajustando_tags <- function(df_diff, debug){
   # word <- "Football (Soccer)"
   # stringi::stri_replace_all_regex(word, "Football \\(Soccer\\)", "Soccer")
   
-  df_diff_ <- df_diff |> 
+  df_diff <- df_diff |> 
     dplyr::mutate(first = stringi::stri_replace_all_regex(first, pattern_first, replacement_first, vectorize_all = F)) |> 
     dplyr::mutate(seccond = stringi::stri_replace_all_regex(seccond, pattern_seccond, replacement_seccond, vectorize_all = F)) |> 
     dplyr::rowwise() |> 
-    dplyr::mutate(add_tags = paste0(union(first, seccond), collapse = ",")) |> 
+    dplyr::mutate(add_tags = paste0(dplyr::union(first, seccond), collapse = ",")) |> 
     dplyr::mutate(add_tags = stringi::stri_replace_all_regex(add_tags, "^,|,$", "", vectorize_all = F))
   
   #### XXXXXXXX ####
   ## Tratar palavras que foram substituídas, mas foram adicionadas em duplicata pelo número de strings
   #### XXXXXXXX ####
+  library(tidyr)
+  df_diff <- df_diff |> 
+    tidyr::separate_longer_delim(add_tags, delim = ",") |> 
+    # tidyr::separate_rows(equal, sep = ",") # |> 
+    dplyr::distinct(id, add_tags, .keep_all = T) |> 
+    # dplyr::arrange(id, add_tags) |> 
+    dplyr::group_by(id) |> 
+    dplyr::mutate(diff_tags = paste0(add_tags, collapse = ",")) |> 
+    dplyr::distinct(id, diff_tags, .keep_all = T) |> 
+    dplyr::ungroup() |> 
+    dplyr::select(id, equal, diff_tags)
   
-  df_diff_ <- df_diff_ |> 
-    dplyr::mutate(all = paste0(equal, ",", add_tags))
-  # df_diff_[] <- lapply(df_diff_, function(x) { attributes(x) <- NULL; x})
-  all_ordered <- df_diff_$all |> 
-    purrr::map(\(x) stringr::str_sort(stringr::str_split_1(x, pattern = ",")))
-  tags <- NULL
-  for(i in 1:length(all_ordered)){
-    pluck(tags, i) <- paste0(pluck(all_ordered, i), collapse = ",")
-  }
-  df_diff_$tags <- as.character(tags)
-  df_diff_ <- df_diff_ |> 
-    dplyr::select(-add_tags, -first, -seccond, -equal, -all) |> 
-    dplyr::ungroup()
+  df_diff <- df_diff |>
+    dplyr::mutate(all_tags = paste0(equal, if_else(diff_tags == "",  "", paste0(",", diff_tags)))) |>
+    tidyr::separate_longer_delim(all_tags, delim = ",") |> 
+    dplyr::arrange(id, all_tags) |>
+    dplyr::group_by(id) |> 
+    dplyr::mutate(tags = paste0(all_tags, collapse = ",")) |> 
+    dplyr::distinct(id, tags, .keep_all = T)|> 
+    dplyr::ungroup() |> 
+    dplyr::select(id, tags)
   
-  df_diff_
+  df_diff
 }
