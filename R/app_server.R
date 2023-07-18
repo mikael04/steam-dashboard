9f492189903a54b133ca11d25500b28c5686fc5d
Mikael <mikael.coletto.eng@gmail.com>
2023-07-18 07:25:30 -0300

    Finalizando alterações para o site com quarto;

--- R/app_server.R
+++ R/app_server.R
@@ -8,14 +8,14 @@ app_server <- function( input, output, session ) {
   # db_collect <- mongo_db |> 
   #   dplyr::collect()
   
-  mongo_db$count()
-  
-  my_collection = mongolite::mongo(collection = "gamesInfo", db = "Steam", url = url_srv) # create connection, database and collection
-  # my_collection$insert(steam_db)
-  
-  vars <- my_collection$distinct("variavel")
-  years <- my_collection$distinct("release_year")
-  languages <- my_collection$distinct("language")
-  
-  df_lang <- as.data.frame(languages)
+mongo_db$count()
+
+my_collection = mongolite::mongo(collection = "gamesInfo", db = "Steam", url = url_srv) # create connection, database and collection
+# my_collection$insert(steam_db)
+
+vars <- my_collection$distinct("variavel")
+years <- my_collection$distinct("release_year")
+languages <- my_collection$distinct("language")
+
+df_lang <- as.data.frame(languages)
 }
